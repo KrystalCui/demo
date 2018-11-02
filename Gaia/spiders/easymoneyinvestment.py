1b1c6c5b3ff0b778f870c9873d2871bb40cde723
di_cui <447859240@qq.com>
2018-11-02 17:09:39 +0800

    11020003

--- Gaia/spiders/easymoneyinvestment.py
+++ Gaia/spiders/easymoneyinvestment.py
@@ -16,60 +16,62 @@ class EasyMoneyInvestment(scrapy.Spider):
         self.ISOTIMEFORMAT = '%Y-%m-%d %X'
 
     def start_requests(self):
-        for i in range(25,1,-1):
+        for i in range(25, 1, -1):
             url = self.url.format(i)
             yield scrapy.Request(url=url, callback= self.parse_easymoneyinvestment, dont_filter = True)
 
     def parse_easymoneyinvestment(self, response):
         uls = response.xpath('.//ul[@id="newsListContent"]')
         for ul in uls:
-            item = EasyMoneyInvestmentItem()
-            item['localtime'] = time.strftime(self.ISOTIMEFORMAT, time.localtime())
-            crawler.info('localtime : %s', item['localtime'])
-            item['titleurl'] = ul.xpath('.//p[@class="title"]/a/@href').extract_first().strip()
-            crawler.info('titleurl : %s',item['titleurl'])
-            item['titletext'] = ul.xpath('.//p[@class="title"]/a/text()').extract_first().strip()
-            crawler.info('titletext : %s',item['titletext'])
-            item['contenttitle'] = ul.xpath('.//p[@class="info"]/@title').extract_first().strip()
-            crawler.info('contenttitle : %s',item['contenttitle'])
-            item['contenttext'] = ul.xpath('.//p[@class="info"]/text()').extract_first().strip()
-            crawler.info('contenttext : %s',item['contenttext'])
-            item['time'] = ul.xpath('.//p[@class="time"]/text()').extract_first().strip()
-            crawler.info('time : %s',item['time'])
-            item['imageurl'] = ul.xpath('.//div[@class="image"]/a/@href').extract_first().strip()
-            crawler.info('imageurl : %s', item['imageurl'])
-            imagescr = ul.xpath('.//div[@class="image"]/a/img/@src').extract_first().strip()
-            item['imagescr'] = ul.xpath('.//div[@class="image"]/a/img/@src').extract_first().strip()
+            lis = ul.xpath('.//li')
+            for li in lis:
+                item = EasyMoneyInvestmentItem()
+                item['localtime'] = time.strftime(self.ISOTIMEFORMAT, time.localtime())
+                crawler.info('localtime : %s', item['localtime'])
+                item['titleurl'] = li.xpath('.//p[@class="title"]/a/@href').extract_first().strip()
+                crawler.info('titleurl : %s',item['titleurl'])
+                item['titletext'] = li.xpath('.//p[@class="title"]/a/text()').extract_first().strip()
+                crawler.info('titletext : %s',item['titletext'])
+                item['contenttitle'] = li.xpath('.//p[@class="info"]/@title').extract_first().strip()
+                crawler.info('contenttitle : %s',item['contenttitle'])
+                item['contenttext'] = li.xpath('.//p[@class="info"]/text()').extract_first().strip()
+                crawler.info('contenttext : %s',item['contenttext'])
+                item['time'] = li.xpath('.//p[@class="time"]/text()').extract_first().strip()
+                crawler.info('time : %s',item['time'])
+                item['imageurl'] = li.xpath('.//div[@class="image"]/a/@href').extract_first().strip()
+                crawler.info('imageurl : %s', item['imageurl'])
+                imagescr = li.xpath('.//div[@class="image"]/a/img/@src').extract_first().strip()
+                item['imagescr'] = li.xpath('.//div[@class="image"]/a/img/@src').extract_first().strip()
 
-            try:
-                self.strwd = os.getcwd() + '\\image'
-                if not os.path.isdir(self.strwd):
-                    os.mkdir('image')
-                namenum = item['imagescr'].rfind('/') + 1
-                self.name = item['imagescr'][namenum:]
-                urllib.request.urlretrieve(item['imagescr'], self.strwd + '\\' + self.name)
-                text = gaia_upload_u('http://gress.gaiafintech.com/upload', imagescr)
-                if len(text) > 0:
-                    textidvalue = text.split(',')
-                    textidsize = len(textidvalue)
-                    for i in range(0,textidsize,1):
-                        idvalues = textidvalue[i].split(':')
-                        if idvalues[0] == '{"dfiles"':
-                            if idvalues[1] != None or '':
-                                item['imagescr'] = idvalues[1].strip('"')
-                                break
-                            else:
-                                crawler.info('%s的图片未上传成功', item['titletext'])
-                                break
-                    crawler.info('imagescr : %s', item['imagescr'])
+                try:
+                    self.strwd = os.getcwd() + '\\image'
+                    if not os.path.isdir(self.strwd):
+                        os.mkdir('image')
+                    namenum = item['imagescr'].rfind('/') + 1
+                    self.name = item['imagescr'][namenum:]
+                    urllib.request.urlretrieve(item['imagescr'], self.strwd + '\\' + self.name)
+                    text = gaia_upload_u('http://gress.gaiafintech.com/upload', imagescr)
+                    if len(text) > 0:
+                        textidvalue = text.split(',')
+                        textidsize = len(textidvalue)
+                        for i in range(0,textidsize,1):
+                            idvalues = textidvalue[i].split(':')
+                            if idvalues[0] == '{"dfiles"':
+                                if idvalues[1] != None or '':
+                                    item['imagescr'] = idvalues[1].strip('"')
+                                    break
+                                else:
+                                    crawler.info('%s的图片未上传成功', item['titletext'])
+                                    break
+                        crawler.info('imagescr : %s', item['imagescr'])
 
 
-            except Exception as e:
-                print(e)
-                crawler.info('%s的图片下载未成功',item['titletext'])
-                continue
+                except Exception as e:
+                    print(e)
+                    crawler.info('%s的图片下载未成功',item['titletext'])
+                    continue
 
-            yield item
+                yield item
 
     # def topost(self):
         # 在 urllib2 上注册 http 流处理句柄
