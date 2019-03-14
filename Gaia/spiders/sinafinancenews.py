99957407c8b88ac0641486f6522dd07269c28b5e
KrystalCui <447859240@qq.com>
2019-03-14 10:29:08 +0800

    修改redis端口号

--- Gaia/spiders/sinafinancenews.py
+++ Gaia/spiders/sinafinancenews.py
@@ -27,7 +27,7 @@ class SinaFinanceNews(scrapy.Spider, Process):
         #redis
         pool = redis.ConnectionPool(
             host=redis_args.get('host'),
-            port=redis_args.get('port'),
+            port='5534',#修改后可进行外网访问
             db=redis_args.get('id_name'),
             password=redis_args.get('password')
         )
